3c10ffe2ce032afb88145f86ba02cdae13d2664f
iaiir <abeyang13@gmail.com>
2019-08-29 16:06:57 -0500

    Made my model and created my database.

---
+++ SuperHeroesProject/Models/SuperHero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeroesProject.Models
+{
+    public class SuperHero
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string alterEgo { get; set; }
+        public string primaryAbility { get; set; }
+        public string secondaryAbility { get; set; }   
+        public string catchPhrase { get; set; }
+    }
+}
